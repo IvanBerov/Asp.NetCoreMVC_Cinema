06b60171c9aef6cebc6211ae7cf6386ccd1c702b
Ivan Berov <ivanberov3@gmail.com>
2022-03-25 11:18:24 +0100

    Add my first model

---
+++ Cinema/Cinema/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
