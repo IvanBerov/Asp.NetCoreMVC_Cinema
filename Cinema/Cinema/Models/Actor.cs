07cfe2e61d3f39eafc2455b995ad097722075da7
Ivan Berov <ivanberov3@gmail.com>
2022-03-28 13:59:42 +0200

    add view actors list

--- Cinema/Cinema/Models/Actor.cs
+++ Cinema/Cinema/Models/Actor.cs
@@ -8,10 +8,13 @@ namespace CinemaApp.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Profile Pictire")]
         public string PictureUrl { get; set; }
 
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
 
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
 
         public List<Actor_Movie> Actors_Movies{ get; set; }
