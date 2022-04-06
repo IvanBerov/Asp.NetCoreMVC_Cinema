5d8a8bb513f82180f993116dd399e208562390ca
Ivan Berov <ivanberov3@gmail.com>
2022-04-06 13:16:43 +0200

    Add OrdersController and shopping card VM

---
+++ Cinema/Cinema/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using CinemaApp.Data.Cart;
+using CinemaApp.Data.Services;
+using CinemaApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartViewModel()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal() 
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ Cinema/Cinema/Data/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CinemaApp.Data.Cart;
+
+namespace CinemaApp.Data.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public double ShoppingCartTotal { get; set; }
+    }
+}
