bff79930086833d5d68cc5150fd2af052ffd002b
Ivan Berov <ivanberov3@gmail.com>
2022-04-07 13:02:24 +0200

    add listing the orders

--- Cinema/Cinema/Controllers/OrdersController.cs
+++ Cinema/Cinema/Controllers/OrdersController.cs
@@ -19,6 +19,15 @@ namespace CinemaApp.Controllers
             _ordersService = ordersService;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
