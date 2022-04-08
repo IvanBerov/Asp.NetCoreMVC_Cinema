c9b8ac1d2f010cf5448e146709f310aaf65a9c5a
Ivan Berov <ivanberov3@gmail.com>
2022-04-08 13:27:44 +0200

    add logout

--- Cinema/Cinema/Controllers/AccountController.cs
+++ Cinema/Cinema/Controllers/AccountController.cs
@@ -90,5 +90,13 @@ namespace CinemaApp.Controllers
 
             return View("RegisterCompleted");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
