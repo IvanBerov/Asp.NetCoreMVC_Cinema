bf8a7c71851493d714240aa98c66b4c114d8d45d
Ivan Berov <ivanberov3@gmail.com>
2022-04-08 11:40:35 +0200

    add login view

---
+++ Cinema/Cinema/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Data.ViewModels;
+using CinemaApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login() => View(new LoginViewModel());
+    }
+}
